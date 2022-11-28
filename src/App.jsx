3cfe0661f939cfe64e9239a1d638454516f221fb
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-28 17:24:00 -0500

    Create PageLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Header, Footer } from "./components/HeaderFooter";
 import { Homepage } from "./pages/Homepage";
 
 import "./styles/app.scss";
 
 export const App = () => (
-  <article>
-    <Header />
-    <Homepage />
-    <Footer />
-  </article>
+
+  <Homepage />
+
 );
