a797558380232d33760c7db0d172fb94087a01c8
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-28 17:58:30 -0500

    Add Example Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ExamplePage } from "./pages/ExamplePage";
 import { Homepage } from "./pages/Homepage";
 
 import "./styles/app.scss";
 
 export const App = () => (
-
-  <Homepage />
-
+  <Router>
+    <Routes>{/* The Routes component decides which component to show based on the current URL.*/}
+      <Route path='/' element={<Homepage />} />
+      <Route path='page/:customPath' element={<ExamplePage />} />
+    </Routes>
+  </Router>
 );
---
+++ src/pages/ExamplePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
+import { PageLayout } from "../components/PageLayout";
+
+/**
+ * This component is an example of a secondary page that can have a dynamic pathname,
+ * and dynamic page metadata. It can also reference the url parameter values within the component.
+ */
+export const ExamplePage = () => {
+  const { customPath } = useParams();
+  return (
+    <PageLayout>
+      <Helmet>
+        {/* Custom page metadata (such as page title or description) goes here:  */}
+        <title>Page: {customPath}</title>
+      </Helmet>
+      <div>
+        <div className="container">
+          <h1 className="headline">Example Secondary Page</h1>
+          <p className="deck">This page can have any dynamic url! Right now, it's found at <code>/{customPath}</code></p>
+        </div>
+      </div>
+    </PageLayout>
+  );
+}
