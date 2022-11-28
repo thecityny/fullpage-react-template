1316f48a626a7a21e51447f25f480e3b6e197311
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-28 14:23:16 -0500

    Make template css framework agnostic
    This PR removes the pre-installed Material UI css kit as well as some icons from Grommet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
 import { Footer, Header } from "./components/HeaderFooter";
 
 import "./styles/app.scss";
@@ -12,46 +11,44 @@ const byline = [
 ];
 
 export const App = () => (
-  <HelmetProvider>
-    <article>
-      <Header />
-      <div>
-        <div className="container">
-          <h1 className="headline">This is not a headline</h1>
-          <div className="attribution">
-            <p className="byline">
-              By{" "}
-              {byline.map((author, i) => (
-                <span key={i} className="author">
-                  <a href={author.url}>{author.name}</a>
-                  {i < byline.length - 2
-                    ? ", "
-                    : i < byline.length - 1
-                    ? " and "
-                    : ""}
-                </span>
-              ))}
-            </p>
-          </div>
-        </div>
-
-        <br />
-
-        <div className="container">
-          <p className="copy">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            pellentesque luctus magna sed auctor. Cras ullamcorper urna mattis
-            lectus sagittis, quis vehicula dui vestibulum.
-          </p>
-          <p className="copy">
-            Aliquam sollicitudin libero vel diam cursus, ut egestas ex
-            scelerisque. Nullam vestibulum metus interdum, dictum erat vel,
-            luctus elit. Nullam aliquam ante elit, mollis aliquet nulla
-            efficitur molestie. Praesent a tempus mi.
+  <article>
+    <Header />
+    <div>
+      <div className="container">
+        <h1 className="headline">This is not a headline</h1>
+        <div className="attribution">
+          <p className="byline">
+            By{" "}
+            {byline.map((author, i) => (
+              <span key={i} className="author">
+                <a href={author.url}>{author.name}</a>
+                {i < byline.length - 2
+                  ? ", "
+                  : i < byline.length - 1
+                  ? " and "
+                  : ""}
+              </span>
+            ))}
           </p>
         </div>
       </div>
-      <Footer />
-    </article>
-  </HelmetProvider>
+
+      <br />
+
+      <div className="container">
+        <p className="copy">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+          pellentesque luctus magna sed auctor. Cras ullamcorper urna mattis
+          lectus sagittis, quis vehicula dui vestibulum.
+        </p>
+        <p className="copy">
+          Aliquam sollicitudin libero vel diam cursus, ut egestas ex
+          scelerisque. Nullam vestibulum metus interdum, dictum erat vel, luctus
+          elit. Nullam aliquam ante elit, mollis aliquet nulla efficitur
+          molestie. Praesent a tempus mi.
+        </p>
+      </div>
+    </div>
+    <Footer />
+  </article>
 );
