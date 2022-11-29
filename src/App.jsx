c62e441a14c11a6274cb3182295769d1f9e85933
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-29 14:23:37 -0500

    Test: Use Hash Router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { ExamplePage } from "./pages/ExamplePage";
 import { Homepage } from "./pages/Homepage";
 
 import "./styles/app.scss";
 
 export const App = () => (
-  <Router>
+  <Router basename="/">
     <Routes>
       {/* The Routes component decides which component to show based on the current URL.*/}
-      <Route
-        path="/fullpage-react-template/multipage/"
-        element={<Homepage />}
-      />
-      <Route
-        path="/fullpage-react-template/multipage/#/pages/:boop"
-        element={<ExamplePage />}
-      />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/page/:customPath" element={<ExamplePage />} />
     </Routes>
   </Router>
 );
