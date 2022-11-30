0b2a5c9239fb832040b357b9fd94508edc0470b4
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-30 11:07:37 -0500

    Add typescript support

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
